fcc637e1900bd86b9e828c40a42cadd66dd8706c
Alexisxde <olivarezalexis749@gmail.com>
2024-11-29 01:16:10 -0300

    fix dependencies

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,12 @@ const envVars = z.object({
   DATABASE_AUTH_TOKEN: z.string(),
   DATABASE_URL: z.string().url(),
   PORT: z.string().optional(),
-  API_KEY: z.string()
+  API_KEY: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  SECRET_SEGURITY: z.string(),
+  NODE_ENV: z.string(),
+  API_URL: z.string().optional()
 })
 
 envVars.parse(process.env)
