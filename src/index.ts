80f665c2698701a79d47e9583fa08865b1b3f34b
Alexisxde <olivarezalexis749@gmail.com>
2024-12-09 23:53:57 -0300

    fix eslint bug

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
+import server from './server'
+
+/** @config */
 import 'dotenv/config'
 import './config'
 import { API_URL } from './config'
+
+/** @middlewares */
+import validateApiKey from './middlewares/ValidateAPIKEY'
+
+/** @routes */
 import Upload from './routes/Upload'
 import Users from './routes/Users'
-import server from './server'
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? '3000'
 
 server.get('/', (_, res) => {
   res.send({
     url: API_URL,
-    users: `/api/users`
+    users: '/api/users'
   })
 })
 
-// ! Falta el middleware de la APIKEY.
-server.use('/api/users', Users)
-server.use('/api', Upload)
+server.use('/api/users', validateApiKey, Users)
+server.use('/api', validateApiKey, Upload)
 
 server.listen(PORT, () => {
   console.log(`[server]: http://localhost:${PORT}`)
